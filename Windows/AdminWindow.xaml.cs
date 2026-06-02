58704b8cf242e47071274520659efbc4f6874885
Анастасия Погарцева <anastasiapogarceva06@gmail.com>
2026-06-02 13:07:31 +1000

    Добавьте файлы проекта.

---
+++ Windows/AdminWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using BreadFactory.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BreadFactory.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+
+            LoadUsers();
+        }
+
+        private void LoadUsers()
+        {
+            dgUsers.ItemsSource =
+                DataBaseEntities
+                .GetContext()
+                .Users
+                .ToList();
+        }
+
+        private void btnRefresh_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            LoadUsers();
+        }
+
+        private void btnUnblock_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Users selectedUser =
+                dgUsers.SelectedItem as Users;
+
+            if (selectedUser == null)
+            {
+                MessageBox.Show(
+                    "Выберите пользователя",
+                    "Предупреждение",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            selectedUser.IsBlocked = false;
+
+            selectedUser.FailedAttempts = 0;
+
+            DataBaseEntities
+                .GetContext()
+                .SaveChanges();
+
+            MessageBox.Show(
+                "Пользователь успешно разблокирован",
+                "Успех",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+
+            LoadUsers();
+        }
+
+        private void btnAdd_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            UserEditWindow window =
+                new UserEditWindow(null);
+
+            window.ShowDialog();
+
+            LoadUsers();
+        }
+
+        private void btnEdit_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            Users selectedUser =
+                dgUsers.SelectedItem as Users;
+
+            if (selectedUser == null)
+            {
+                MessageBox.Show(
+                    "Выберите пользователя",
+                    "Предупреждение",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            UserEditWindow window =
+                new UserEditWindow(selectedUser);
+
+            window.ShowDialog();
+
+            LoadUsers();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Users selectedUser = dgUsers.SelectedItem as Users;
+            if (selectedUser == null)
+            {
+                MessageBox.Show(
+                    "Выберите пользователя",
+                    "Предупреждение",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+            
+        }
+    }
+}
---
+++ Windows/UserEditWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using BreadFactory.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BreadFactory.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для UserEditWindow.xaml
+    /// </summary>
+    public partial class UserEditWindow : Window
+    {
+        private Users currentUser;
+
+        public UserEditWindow(Users user)
+        {
+            InitializeComponent();
+
+            if (user == null)
+            {
+                this.Title = "Хлебобулочный завод - добавление";
+            }
+            else this.Title = "Хлебобулочный завод - редактирование";
+
+            cbRole.ItemsSource =
+                DataBaseEntities
+                .GetContext()
+                .Roles
+                .ToList();
+
+            if (user == null)
+            {
+                currentUser = new Users();
+            }
+            else
+            {
+                currentUser = user;
+
+                tbSecondName.Text =
+                    currentUser.UserSecondName;
+
+                tbFirstName.Text =
+                    currentUser.UserFirstName;
+
+                tbPatronymic.Text =
+                    currentUser.UserPatronymic;
+
+                tbLogin.Text =
+                    currentUser.Login;
+
+                pbPassword.Password =
+                    currentUser.PasswordHash;
+
+                chBlocked.IsChecked =
+                    currentUser.IsBlocked;
+
+                cbRole.SelectedValue =
+                    currentUser.UserRoleKey;
+            }
+        }
+
+        private void BtnSave_Click(
+            object sender,
+            RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbSecondName.Text) ||
+                string.IsNullOrWhiteSpace(tbFirstName.Text) ||
+                string.IsNullOrWhiteSpace(tbLogin.Text) ||
+                string.IsNullOrWhiteSpace(pbPassword.Password) ||
+                cbRole.SelectedItem == null)
+            {
+                MessageBox.Show(
+                    "Заполните обязательные поля",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            bool loginExists =
+                DataBaseEntities
+                .GetContext()
+                .Users
+                .Any(x =>
+                    x.Login == tbLogin.Text &&
+                    x.UserID != currentUser.UserID);
+
+            if (loginExists)
+            {
+                MessageBox.Show(
+                    "Пользователь с таким логином уже существует",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+
+                return;
+            }
+
+            currentUser.UserSecondName =
+                tbSecondName.Text;
+
+            currentUser.UserFirstName =
+                tbFirstName.Text;
+
+            currentUser.UserPatronymic =
+                tbPatronymic.Text;
+
+            currentUser.Login =
+                tbLogin.Text;
+
+            currentUser.PasswordHash =
+                pbPassword.Password;
+
+            currentUser.UserRoleKey =
+                (int)cbRole.SelectedValue;
+
+            currentUser.IsBlocked =
+                chBlocked.IsChecked ?? false;
+
+            if (currentUser.UserID == 0)
+            {
+                currentUser.CreatedAt =
+                    System.DateTime.Now;
+
+                DataBaseEntities
+                    .GetContext()
+                    .Users
+                    .Add(currentUser);
+            }
+
+            DataBaseEntities
+                .GetContext()
+                .SaveChanges();
+
+            MessageBox.Show(
+                "Данные успешно сохранены",
+                "Успех",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+
+            DialogResult = true;
+
+            Close();
+        }
+    }
+}
